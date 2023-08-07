92b195cc18d4f6e5895528aa24d76c18de88c125
HyunsooZo <bzhs1992@icloud.com>
2023-08-07 13:14:03 +0900

    chore : 초기 환경설정

---
+++ src/main/java/com/grablunchtogether/GrabLunchTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.grablunchtogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrabLunchTogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrabLunchTogetherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/grablunchtogether/GrabLunchTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grablunchtogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrabLunchTogetherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
