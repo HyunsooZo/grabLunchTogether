ee66047d2f4d3930af0c1f21d7f535fad497f6e4
HyunsooZo <bzhs1992@icloud.com>
2023-08-07 23:33:58 +0900

    feat : 회원가입 예외 추가 (UserSignUpException)
    - 회원가입 시 받은 이메일로 UserRepository 조회,
      회원 정보가 존재 할 경우 기존회원 이므로 해당 예외 던지기

---
+++ src/main/java/com/grablunchtogether/common/exception/UserSignUpException.java
@@ -0,0 +1,7 @@
+package com.grablunchtogether.common.exception;
+
+public class UserSignUpException extends RuntimeException {
+    public UserSignUpException(String message) {
+        super(message);
+    }
+}
