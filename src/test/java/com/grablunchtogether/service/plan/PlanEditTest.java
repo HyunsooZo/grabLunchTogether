7b753ceaf6f221592845e6625b69ffb5b329cc76
HyunsooZo <bzhs1992@icloud.com>
2023-08-09 01:49:05 +0900

    test : 요청중인 상태의 점심약속 수정 테스트코드 작성
    TestEditPlan_Success() -> 점심약속 수정 성공
    TestEditPlan_Fail_AlreadyDone() -> REQUESTED 상태가 아닌 Plan 일 시 예외
    TestEditPlan_Fail_NotMyPlan() -> 내가 요청한 Plan이 아닐 시 예외

---
+++ src/test/java/com/grablunchtogether/service/plan/PlanEditTest.java
@@ -0,0 +1,155 @@
+package com.grablunchtogether.service.plan;
+
+import com.grablunchtogether.common.exception.AuthorityException;
+import com.grablunchtogether.common.results.serviceResult.ServiceResult;
+import com.grablunchtogether.domain.Plan;
+import com.grablunchtogether.domain.User;
+import com.grablunchtogether.dto.plan.PlanCreationInput;
+import com.grablunchtogether.repository.PlanRepository;
+import com.grablunchtogether.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+import static com.grablunchtogether.common.enums.PlanStatus.COMPLETED;
+import static com.grablunchtogether.common.enums.PlanStatus.REQUESTED;
+
+class PlanEditTest {
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private PlanRepository planRepository;
+
+    private PlanService planService;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        planService = new PlanServiceImpl(userRepository, planRepository);
+    }
+
+    @Test
+    public void TestEditPlan_Success() {
+        //given
+        User requester = new User();
+        requester.setId(1L);
+
+        User accepter = new User();
+        accepter.setId(2L);
+
+        Plan plan = Plan.builder()
+                .id(1L)
+                .requester(requester)
+                .accepter(accepter)
+                .planRestaurant("a")
+                .planMenu("a")
+                .planTime(LocalDateTime.now())
+                .requestMessage("a")
+                .planStatus(REQUESTED)
+                .RegisteredAt(LocalDateTime.now())
+                .build();
+
+        PlanCreationInput planModificationInput =
+                PlanCreationInput.builder()
+                        .planRestaurant("aa")
+                        .planMenu("ss")
+                        .planTime(LocalDateTime.now())
+                        .requestMessage("123123")
+                        .build();
+
+        Mockito.when(planRepository.findById(requester.getId()))
+                .thenReturn(Optional.of(plan));
+
+        //when
+        ServiceResult result = planService.editPlanRequest(requester.getId(), plan.getId(), planModificationInput);
+
+        //then
+        Assertions.assertThat(result.isResult()).isTrue();
+        Assertions.assertThat(plan.getRequestMessage()).isEqualTo("123123");
+    }
+
+    @Test
+    public void TestEditPlan_Fail_AlreadyDone() {
+        //given
+        User requester = new User();
+        requester.setId(1L);
+
+        User accepter = new User();
+        accepter.setId(2L);
+
+        Plan plan = Plan.builder()
+                .id(1L)
+                .requester(requester)
+                .accepter(accepter)
+                .planRestaurant("a")
+                .planMenu("a")
+                .planTime(LocalDateTime.now())
+                .requestMessage("a")
+                .planStatus(COMPLETED)
+                .RegisteredAt(LocalDateTime.now())
+                .build();
+
+        PlanCreationInput planModificationInput =
+                PlanCreationInput.builder()
+                        .planRestaurant("aa")
+                        .planMenu("ss")
+                        .planTime(LocalDateTime.now())
+                        .requestMessage("123123")
+                        .build();
+
+        Mockito.when(planRepository.findById(requester.getId()))
+                .thenReturn(Optional.of(plan));
+
+        //when,then
+        Assertions.assertThatThrownBy(() ->
+                        planService.editPlanRequest(requester.getId(), plan.getId(), planModificationInput))
+                .isInstanceOf(AuthorityException.class)
+                .hasMessage("요청중인 상태의 점심약속만 수정할 수 있습니다.");
+    }
+
+    @Test
+    public void TestEditPlan_Fail_NotMyPlan() {
+        //given
+        User requester = new User();
+        requester.setId(1L);
+
+        User accepter = new User();
+        accepter.setId(2L);
+
+        Plan plan = Plan.builder()
+                .id(1L)
+                .requester(accepter)
+                .accepter(requester)
+                .planRestaurant("a")
+                .planMenu("a")
+                .planTime(LocalDateTime.now())
+                .requestMessage("a")
+                .planStatus(COMPLETED)
+                .RegisteredAt(LocalDateTime.now())
+                .build();
+
+        PlanCreationInput planModificationInput =
+                PlanCreationInput.builder()
+                        .planRestaurant("aa")
+                        .planMenu("ss")
+                        .planTime(LocalDateTime.now())
+                        .requestMessage("123123")
+                        .build();
+
+        Mockito.when(planRepository.findById(requester.getId()))
+                .thenReturn(Optional.of(plan));
+
+        //when,then
+        Assertions.assertThatThrownBy(() ->
+                        planService.editPlanRequest(requester.getId(), plan.getId(), planModificationInput))
+                .isInstanceOf(AuthorityException.class)
+                .hasMessage("내가 신청한 약속만 수정할 수 있습니다.");
+    }
+}
