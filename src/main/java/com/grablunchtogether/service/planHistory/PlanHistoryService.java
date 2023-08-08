61a3244b2dc5b8aac0ded8bcdbde11f487c58f08
HyunsooZo <bzhs1992@icloud.com>
2023-08-09 05:32:06 +0900

    feat : PlanHistory 조회기능 추가
    - userId 로 PlanHistory 조회 시 ,
      해당 userId를 accepterId 또는 requester로 PlanHistory 조회하고
      해당 PlanHistory의 Plan 을 PlanDto에 담아 리스트로 반환
    - 빈 리스트일 경우 -> ContentNotFoundException 예외

--- src/main/java/com/grablunchtogether/service/planHistory/PlanHistoryService.java
+++ src/main/java/com/grablunchtogether/service/planHistory/PlanHistoryService.java
@@ -1,16 +1,17 @@
 package com.grablunchtogether.service.planHistory;
 
+import com.grablunchtogether.common.results.serviceResult.ServiceResult;
 import com.grablunchtogether.domain.Plan;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface PlanHistoryService {
-    @Transactional
+
     @Scheduled(cron = "0 * * * * *")
     void updatePlanHistory();
 
-    @Transactional
     void registerHistory(List<Plan> plans);
+
+    ServiceResult listMyHistory(Long userId);
 }
--- src/main/java/com/grablunchtogether/service/planHistory/PlanHistoryServiceImpl.java
+++ src/main/java/com/grablunchtogether/service/planHistory/PlanHistoryServiceImpl.java
@@ -1,9 +1,15 @@
 package com.grablunchtogether.service.planHistory;
 
+import com.grablunchtogether.common.exception.ContentNotFoundException;
+import com.grablunchtogether.common.exception.UserInfoNotFoundException;
+import com.grablunchtogether.common.results.serviceResult.ServiceResult;
 import com.grablunchtogether.domain.Plan;
 import com.grablunchtogether.domain.PlanHistory;
+import com.grablunchtogether.domain.User;
+import com.grablunchtogether.dto.plan.PlanDto;
 import com.grablunchtogether.repository.PlanHistoryRepository;
 import com.grablunchtogether.repository.PlanRepository;
+import com.grablunchtogether.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -11,6 +17,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @EnableScheduling
@@ -19,6 +26,7 @@ import java.util.List;
 public class PlanHistoryServiceImpl implements PlanHistoryService {
     private final PlanHistoryRepository planHistoryRepository;
     private final PlanRepository planRepository;
+    private final UserRepository userRepository;
 
     @Override
     @Transactional
@@ -50,5 +58,36 @@ public class PlanHistoryServiceImpl implements PlanHistoryService {
                         .build());
             }
         });
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public ServiceResult listMyHistory(Long userId) {
+
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new UserInfoNotFoundException("고객 정보를 찾을 수 없습니다.")
+        );
+
+        List<PlanHistory> requestedList =
+                planHistoryRepository.findByRequesterId(user);
+
+        List<PlanHistory> acceptedList =
+                planHistoryRepository.findByAccepterId(user);
+
+        List<PlanDto> result = new ArrayList<>();
+
+        requestedList.forEach(planHistory -> {
+            result.add(PlanDto.of(planHistory.getPlanId()));
+        });
+
+        acceptedList.forEach(planHistory -> {
+            result.add(PlanDto.of(planHistory.getPlanId()));
+        });
+
+        if(result.isEmpty()){
+            throw new ContentNotFoundException("히스토리가 존재하지 않습니다.");
+        }
+
+        return ServiceResult.success("목록 가져오기 성공", result);
     }
 }
