ab11ed14f054f73b6f4b6de4e4364359c367e91d
HyunsooZo <bzhs1992@icloud.com>
2023-09-06 05:35:19 +0900

    feat : Plan Dto 통합(이너클래스활용) 및 서비스로직 개선
    - Dto를 하나로 통합하여 관리
    - 불필요한 예외 처리 삭제 및 스트림을 사용한 코드 개선

--- src/main/java/com/grablunchtogether/dto/plan/PlanDto.java
+++ src/main/java/com/grablunchtogether/dto/plan/PlanDto.java
@@ -1,41 +1,79 @@
 package com.grablunchtogether.dto.plan;
 
-import com.grablunchtogether.enums.PlanStatus;
 import com.grablunchtogether.domain.Plan;
+import com.grablunchtogether.enums.PlanStatus;
+import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
+import java.util.List;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
+
 public class PlanDto {
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    @ApiModel("Plan 요청")
+    public static class Request {
+        @NotBlank(message = "메뉴는 필수입력 항목입니다.")
+        private String planMenu;
 
-    private long id;
-    private long requesterId;
-    private long accepterId;
-    private String requesterEmail;
-    private String accepterEmail;
-    private String planMenu;
-    private String planRestaurant;
-    private PlanStatus planStatus;
-    private LocalDateTime planTime;
+        @NotBlank(message = "식당이름은 필수입력 항목입니다.")
+        private String planRestaurant;
 
-    public static PlanDto of(Plan plan) {
-        return PlanDto.builder()
-                .id(plan.getId())
-                .requesterId(plan.getRequester().getId())
-                .accepterId(plan.getAccepter().getId())
-                .requesterEmail(plan.getRequester().getUserEmail())
-                .accepterEmail(plan.getAccepter().getUserEmail())
-                .planMenu(plan.getPlanMenu())
-                .planRestaurant(plan.getPlanRestaurant())
-                .planStatus(plan.getPlanStatus())
-                .planTime(plan.getPlanTime())
-                .build();
+        private String requestMessage;
+
+        @DateTimeFormat(pattern = "yyyy-MM-dd hh:mm")
+        private LocalDateTime planTime;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    @ApiModel("Plan Dto")
+    public static class Dto {
+        private long id;
+        private long requesterId;
+        private long accepterId;
+        private String requesterEmail;
+        private String accepterEmail;
+        private String planMenu;
+        private String planRestaurant;
+        private PlanStatus planStatus;
+        private LocalDateTime planTime;
+
+        public static Dto of(Plan plan) {
+            return Dto.builder()
+                    .id(plan.getId())
+                    .requesterId(plan.getRequester().getId())
+                    .accepterId(plan.getAccepter().getId())
+                    .requesterEmail(plan.getRequester().getUserEmail())
+                    .accepterEmail(plan.getAccepter().getUserEmail())
+                    .planMenu(plan.getPlanMenu())
+                    .planRestaurant(plan.getPlanRestaurant())
+                    .planStatus(plan.getPlanStatus())
+                    .planTime(plan.getPlanTime())
+                    .build();
+        }
+    }
+
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    @ApiModel("Plan 응답")
+    public static class Response {
+        private List<Dto> plans;
+
+        public static Response of(List<Dto> plans) {
+            return Response.builder().plans(plans).build();
+        }
     }
 }
--- src/main/java/com/grablunchtogether/service/PlanService.java
+++ src/main/java/com/grablunchtogether/service/PlanService.java
@@ -1,12 +1,10 @@
-package com.grablunchtogether.service.plan;
+package com.grablunchtogether.service;
 
-import com.grablunchtogether.exception.CustomException;
-import com.grablunchtogether.exception.ErrorCode;
-import com.grablunchtogether.common.results.serviceResult.ServiceResult;
 import com.grablunchtogether.domain.Plan;
 import com.grablunchtogether.domain.User;
-import com.grablunchtogether.dto.plan.PlanCreationInput;
 import com.grablunchtogether.dto.plan.PlanDto;
+import com.grablunchtogether.exception.CustomException;
+import com.grablunchtogether.exception.ErrorCode;
 import com.grablunchtogether.repository.PlanRepository;
 import com.grablunchtogether.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,12 +13,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
-import static com.grablunchtogether.exception.ErrorCode.*;
 import static com.grablunchtogether.enums.PlanStatus.*;
+import static com.grablunchtogether.exception.ErrorCode.*;
 
 @RequiredArgsConstructor
 @Service
@@ -29,9 +27,9 @@ public class PlanService {
     private final PlanRepository planRepository;
 
     @Transactional
-    public ServiceResult createPlan(Long userId,
-                                    Long accepterId,
-                                    PlanCreationInput planCreationInput) {
+    public void createPlan(Long userId,
+                           Long accepterId,
+                           PlanDto.Request planCreationRequest) {
 
         User requester = userRepository.findById(userId)
                 .orElseThrow(() -> new CustomException(USER_INFO_NOT_FOUND));
@@ -49,62 +47,45 @@ public class PlanService {
                 Plan.builder()
                         .requester(requester)
                         .accepter(accepter)
-                        .planRestaurant(planCreationInput.getPlanRestaurant())
-                        .planMenu(planCreationInput.getPlanMenu())
-                        .planTime(planCreationInput.getPlanTime())
-                        .requestMessage(planCreationInput.getRequestMessage())
+                        .planRestaurant(planCreationRequest.getPlanRestaurant())
+                        .planMenu(planCreationRequest.getPlanMenu())
+                        .planTime(planCreationRequest.getPlanTime())
+                        .requestMessage(planCreationRequest.getRequestMessage())
                         .planStatus(REQUESTED)
                         .build()
         );
-
-        return ServiceResult.success("약속이 생성되었습니다.");
     }
 
     //나에게 요청된 점심약속 목록가져오기
     @Transactional(readOnly = true)
-    public ServiceResult getPlanListReceived(Long userId) {
+    public List<PlanDto.Dto> getPlanListReceived(Long userId) {
 
-        List<PlanDto> result = new ArrayList<>();
-
-        List<Plan> list =
+        List<Plan> plans =
                 planRepository.findByAccepterIdAndPlanStatus(userId, REQUESTED);
 
-        if (list.isEmpty()) {
-            throw new CustomException(CONTENT_NOT_FOUND);
-        }
-
-        list.forEach(plan -> {
-            result.add(PlanDto.of(plan));
-        });
-
-        return ServiceResult.success("목록 수신 성공", result);
+        return plans.stream()
+                .map(PlanDto.Dto::of)
+                .collect(Collectors.toList());
     }
 
     //내가 요청한 점심약속 목록 조회
     @Transactional(readOnly = true)
-    public ServiceResult getPlanListIRequested(Long userId) {
-        List<PlanDto> result = new ArrayList<>();
+    public List<PlanDto.Dto> getPlanListIRequested(Long userId) {
 
-        List<Plan> list =
+        List<Plan> plans =
                 planRepository.findByRequesterIdAndPlanStatusNot(userId, COMPLETED);
 
-        if (list.isEmpty()) {
-            throw new CustomException(CONTENT_NOT_FOUND);
-        }
-
-        list.forEach(plan -> {
-            result.add(PlanDto.of(plan));
-        });
-
-        return ServiceResult.success("목록 수신 성공", result);
+        return plans.stream()
+                .map(PlanDto.Dto::of)
+                .collect(Collectors.toList());
     }
 
     //받은 요청을 수락(Y) 또는 거절(N)
     @Transactional
-    public ServiceResult approvePlan(Long userId, Long planId, Character acceptCode) {
+    public void approvePlan(Long userId, Long planId, Character acceptCode) {
 
         Plan plan = planRepository.findByIdAndAccepterId(planId, userId)
-                .orElseThrow(() -> new CustomException(CONTENT_NOT_FOUND));
+                .orElseThrow(() -> new CustomException(PLAN_NOT_FOUND));
 
         if (!plan.getPlanStatus().equals(REQUESTED)) {
             throw new CustomException(NOT_PERMITTED);
@@ -113,16 +94,14 @@ public class PlanService {
         plan.approve(acceptCode);
 
         planRepository.save(plan);
-
-        return ServiceResult.success(acceptCode == 'Y' ? "수락이 완료되었습니다." : "거절이 완료되었습니다.");
     }
 
     // 받은 or 신청한 요청을 취소
     @Transactional
-    public ServiceResult cancelPlan(Long userid, Long planId) {
+    public void cancelPlan(Long userid, Long planId) {
 
-        Plan plan = planRepository.findById(planId).orElseThrow(()
-                -> new CustomException(CONTENT_NOT_FOUND));
+        Plan plan = planRepository.findById(planId)
+                .orElseThrow(() -> new CustomException(PLAN_NOT_FOUND));
 
         if (!Objects.equals(plan.getAccepter().getId(), userid) &&
                 !Objects.equals(plan.getRequester().getId(), userid)) {
@@ -136,18 +115,16 @@ public class PlanService {
         plan.cancel();
 
         planRepository.save(plan);
-
-        return ServiceResult.success("점심약속요청이 취소되었습니다.");
     }
 
     //내가 요청한 점심약속 수정
     @Transactional
-    public ServiceResult editPlanRequest(Long userId,
-                                         Long planId,
-                                         PlanCreationInput planModificationInput) {
+    public void editPlanRequest(Long userId,
+                                Long planId,
+                                PlanDto.Request planModificationRequest) {
 
         Plan plan = planRepository.findById(planId)
-                .orElseThrow(() -> new CustomException(CONTENT_NOT_FOUND));
+                .orElseThrow(() -> new CustomException(PLAN_NOT_FOUND));
 
         if (!Objects.equals(plan.getRequester().getId(), userId)) {
             throw new CustomException(NOT_PERMITTED);
@@ -157,21 +134,16 @@ public class PlanService {
             throw new CustomException(ErrorCode.CAN_NOT_EDIT_PLAN);
         }
 
-        plan.update(planModificationInput.getPlanMenu(),
-                planModificationInput.getPlanRestaurant(),
-                planModificationInput.getPlanTime(),
-                planModificationInput.getRequestMessage());
+        plan.update(planModificationRequest);
 
         planRepository.save(plan);
-
-        return ServiceResult.success("점심약속 요청 수정이 완료되었습니다.");
     }
 
     //점심약속 삭제
     @Transactional
-    public ServiceResult planDeletion(Long userid, Long planId) {
-        Plan plan = planRepository.findById(planId).orElseThrow(()
-                -> new CustomException(CONTENT_NOT_FOUND));
+    public void planDeletion(Long userid, Long planId) {
+        Plan plan = planRepository.findById(planId)
+                .orElseThrow(() -> new CustomException(PLAN_NOT_FOUND));
 
         if (!Objects.equals(plan.getRequester().getId(), userid)) {
             throw new CustomException(NOT_PERMITTED);
@@ -190,7 +162,5 @@ public class PlanService {
         }
 
         planRepository.delete(plan);
-
-        return ServiceResult.success("점심약속요청이 삭제되었습니다.");
     }
 }
