5b89cc2cebd453dac410dece9ed9ba23617f6e94
HyunsooZo <bzhs1992@icloud.com>
2023-09-08 01:13:24 +0900

    style : 누락되었던 Repository 어노테이션 추가

--- src/main/java/com/grablunchtogether/repository/BookmarkSpotRepository.java
+++ src/main/java/com/grablunchtogether/repository/BookmarkSpotRepository.java
@@ -3,10 +3,12 @@ package com.grablunchtogether.repository;
 import com.grablunchtogether.domain.BookmarkSpot;
 import com.grablunchtogether.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface BookmarkSpotRepository extends JpaRepository<BookmarkSpot, Long> {
     List<BookmarkSpot> findByUserId(User user);
 }
--- src/main/java/com/grablunchtogether/repository/MustEatPlaceRepository.java
+++ src/main/java/com/grablunchtogether/repository/MustEatPlaceRepository.java
@@ -2,9 +2,11 @@ package com.grablunchtogether.repository;
 
 import com.grablunchtogether.domain.MustEatPlace;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface MustEatPlaceRepository extends JpaRepository<MustEatPlace,Long> {
     List<MustEatPlace> findByCityOrderByRateDesc(String cityName);
 }
