eb2e241347c4c02cac37c2b60062b28ddf57920d
HyunsooZo <bzhs1992@icloud.com>
2023-10-10 14:25:24 +0900

    feat : 웹소켓 적용부분 삭제

--- src/main/java/com/grablunchtogether/repository/ChatMessageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.grablunchtogether.repository;
-
-import com.grablunchtogether.domain.ChatMessage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatMessageRepository extends JpaRepository<ChatMessage,Long> {
-}
